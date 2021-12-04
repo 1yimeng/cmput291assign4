e526846b85c160e695801393d0a8f7670585b5ce
1yimeng <yimwang6@gmail.com>
2021-12-04 14:55:37 -0700

    cleaned q1

--- A4Q1Norm.py
+++ A4Q1Norm.py
@@ -1,6 +1,9 @@
 # db.artists.find({ num_tracks: { $gte: 1}}, {followers: 0, genres: 0, popularity: 0})
 # db.artists.aggregate([{$addFields: {"num_tracks": { $size: "$tracks"}}}])
 #  db.artists.aggregate([{$addFields: {"num_tracks": { $size: "$tracks"}}}, {$project : { followers: 0, genres: 0, popularity: 0, tracks: 0}}])
+
+
+# -.- embed ver
 from pymongo import MongoClient
 
 client = MongoClient('mongodb://localhost:27012')
@@ -11,15 +14,29 @@ db = client["A4dbNorm"]
 artists_doc = db["artists"]
 tracks_doc = db["tracks"]
 
-add_numtracks = { "$addFields": {"num_tracks": { "$size": "$tracks"}}}
+add_numtracks = { "$addFields": {"num_tracks": { "$size": "$mytracks"}}}
 
 projection = {"$project" : { "artist_id": 1, "name": 1, "num_tracks": 1}}
 
-numtrack_return = artists_doc.aggregate([add_numtracks, projection])
+# numtrack_return = artists_doc.aggregate([add_numtracks, projection])
 
-for x in numtrack_return:
+# for x in numtrack_return:
+    # if x["num_tracks"] >= 1:
+        # print(x)
+
+result = artists_doc.aggregate([
+    {'$lookup': {'from': 'tracks',
+                 'localField': 'tracks',
+                 'foreignField': 'track_id',
+                 'as': 'mytracks'}},
+    add_numtracks,
+    projection,
+])
+
+for x in result:
     if x["num_tracks"] >= 1:
         print(x)
+
 
 
 
