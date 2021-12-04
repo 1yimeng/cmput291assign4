d338f9e52ec039839db35e215eb78b2e6dccc292
Anushka Khare <71386735+AnushkaKhare-Eng@users.noreply.github.com>
2021-12-04 15:41:21 -0700

    Q3 normal

---
+++ A4Q3.py
@@ -0,0 +1,20 @@
+import pymongo
+import datetime
+from bson.json_util import loads
+from pymongo import collection
+import mongodb_example
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+db = client["A4dbNorm"]
+artists_doc = db["artists"]
+tracks_doc = db["tracks"]
+tracks_doc.aggregate([{"$unwind": "$tracks"}])
+tracks_cursor = tracks_doc.aggregate({
+    "$group": {
+        "_id": "artist_id",
+        "total_length": {"$sum": "$duration"}},
+    "artist_id": "artist_id"
+
+})
+for entry in tracks_cursor:
+    print(entry)
