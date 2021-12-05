4cfecdacfd4eba5522e6eb7aa9b11691f370307e
elsachen0907 <74115079+elsachen0907@users.noreply.github.com>
2021-12-05 00:21:11 -0700

    done q1 embed

--- A4Q1Embed.py
+++ A4Q1Embed.py
@@ -1,18 +1,14 @@
 from pymongo import MongoClient
+client = MongoClient()
 
-client = MongoClient('mongodb://localhost:27012')
-
-db = client["A4dbNorm"]
-
-# Create or open the collection in the db
-artists_doc = db["artists"]
-tracks_doc = db["tracks"]
+db = client["A4dbEmbed"]
+artiststracks = db['ArtistsTracks']
 
 add_numtracks = { "$addFields": {"num_tracks": { "$size": "$tracks"}}}
 
 projection = {"$project" : { "artist_id": 1, "name": 1, "num_tracks": 1}}
 
-numtrack_return = artists_doc.aggregate([add_numtracks, projection])
+numtrack_return = artiststracks.aggregate([add_numtracks, projection])
 
 for x in numtrack_return:
     if x["num_tracks"] >= 1:
