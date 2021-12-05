39dca046b396f4f42606702a01021c72d2bc32fe
elsachen0907 <74115079+elsachen0907@users.noreply.github.com>
2021-12-05 00:20:26 -0700

    done

--- A4Q2Embed.py
+++ A4Q2Embed.py
@@ -4,17 +4,6 @@ client = MongoClient()
 db = client["A4dbEmbed"]
 artiststracks = db['ArtistsTracks']
 
-#     {'$group': {'_id': 
-#     {'new_track_id':{'$track':{'$track_id':{'$regex': "^70"}}}},
-#                  'average':{'$avg':"$danceability"}}
-#     }])
-
-    # artiststracks.aggregate([
-    # {'$group': {'_id': 
-    # {'new_track_id':{'$tracks':{'$track_id':{'$regex': "^70"}}}},
-    #              'average':{'$avg':"$danceability"}}
-    # }])
-
 cursor = artiststracks.aggregate([
     {'$unwind':'$tracks'},
     {'$match':{'tracks.track_id':{'$regex': "^70.*"}}},
@@ -23,13 +12,6 @@ cursor = artiststracks.aggregate([
     }
     ])
 
-    
-# print(new_output)    
-# print(list(cursor))  
+ 
 for document in cursor:
     print(document)
-#     {$group:
-# {_id:
-# {new_track_id:{"$regex: /^70/"}}, 
-# average:{$avg:"$danceability"}
-# }}])
