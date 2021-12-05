35b1fdb45a9d0b7f2cdb73093adaf78ca649ebf4
Anushka Khare <71386735+AnushkaKhare-Eng@users.noreply.github.com>
2021-12-05 12:49:32 -0700

    updated

--- A4Q4.py
+++ A4Q4.py
@@ -4,21 +4,26 @@ from bson.json_util import loads
 from pymongo import collection
 import mongodb_example
 from pymongo import MongoClient
-client = MongoClient('mongodb://localhost:27012')
+client = MongoClient('localhost', 27017)
 
 # Create or open the video_store database on server.
 db = client["A4dbNorm"]
-artists_doc = db["artists"]
-tracks_doc = db["tracks"]
-# Note: in the query 4 exapmple .datetime is used to save it
+Artists = db["artists"]
+Tracks = db["tracks"]
+newCollection = Artists.aggregate([
+    {'$unwind': '$tracks'},
+    {'$lookup': {'from': 'Tracks'},
+     '$match': {"$eq": ["$tracks", "$track_id"],
+                "$gt": ["$release_date", datetime.datetime(1950, 1, 1)]}},
+    {"$project": {"_id": "$_id", "name": "$name", "t_name": "$tracks.name",
+                  "t_release_date": "$tracks.release_date"}}
 
-tracksCursor = tracks_doc.find(
-    {"release_date": {"$gt": datetime.datetime(1950, 1, 1)}})
-# newCollection = db["Tracks1"]
-for entry in tracksCursor:
+])
+
+
+for entry in newCollection:
     # newCollection.insertOne(tracksCursor.next())
 
-    artists_doc.find({"artist_id": entry["artist_id"]})
     print(entry)
 
 
