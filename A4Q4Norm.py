c42223fd58d75c0142b89ae6a7205affc94d631c
Anushka Khare <71386735+AnushkaKhare-Eng@users.noreply.github.com>
2021-12-06 12:10:53 -0700

    comments updated

--- A4Q4Norm.py
+++ A4Q4Norm.py
@@ -4,7 +4,12 @@ import datetime
 client = MongoClient('localhost', 27017)
 db = client["A4dbNorm"]
 artistColl = db["artists"]
-
+#Creating a join between the tracks and artist tables and getting all 
+#tracks for each artist by using unwind 
+#the match field ensures that we get the values that have release dates greater
+# than 1/1/1950.
+# once we obtain the entires with release date greater than 1/1/1950 we project the relevant information
+#such as objectid, name, track name, track release date 
 output = artistColl.aggregate([
     {'$lookup': {'from': 'tracks',
                  'localField': 'tracks',
@@ -21,6 +26,6 @@ output = artistColl.aggregate([
         "t_release_date": "$tracks.release_date"
     }}
 ])
-
+#printing the results obtained
 for entry in output:
     print(entry)
