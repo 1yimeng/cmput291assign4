80f767b73ae77b516d012045bbc63944b4c09fcf
Anushka Khare <71386735+AnushkaKhare-Eng@users.noreply.github.com>
2021-12-06 12:14:36 -0700

    comments for q3

--- A4Q3Norm.py
+++ A4Q3Norm.py
@@ -4,7 +4,11 @@ client = MongoClient('localhost', 27017)
 
 db = client["A4dbNorm"]
 artistsColl = db["artists"]
-
+#joining the artist and tracks then we consider all the tracks in the 
+#artists, we group by artist id, so that we can group all the songs by one artist
+# we then sum the length of all the songs. 
+# when printing the output we ensure that the artist field is also included twice in 
+# the final output. 
 output = artistsColl.aggregate([
     {'$lookup': {
         'from': 'tracks',
